b139b8a2ee68646610d07963f4d1a425d0ece727
Allan Meneses <allanmeneses.ec@gmail.com>
2017-04-15 00:33:51 -0300

    criacao de menu com duas opcoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,26 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { MenuTeste } from '../pages/menu-teste/menu-teste';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  pages: Array<{component: any, title: string, icon: string}>;
+
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, 
+              statusBar: StatusBar, 
+              splashScreen: SplashScreen) {
+
+    this.pages= [
+      {component:HomePage, title: 'Home', icon:'home'},
+      {component:MenuTeste, title: 'Menu Teste', icon:'menu'}
+    ];
+
+    
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -18,5 +31,8 @@ export class MyApp {
       splashScreen.hide();
     });
   }
-}
 
+  openPage(page: any ) : void {
+    this.rootPage = page.component;
+  }
+}
